94cf9c77d575d2bbe1bc5b6b9d1ae21e1c0fbb3f
Durgesh Thakur <durgesh.bisen10102@gmail.com>
2025-01-20 13:11:29 +0530

    Add Cart logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
 
 const Navbar = () => {
+  const [cartItemsCount, setCartItemsCount] = useState(0);
   const navigate = useNavigate();
+
+  const calculateCartItemsCount = () => {
+    const storedCart = JSON.parse(localStorage.getItem('cart'));
+    if (storedCart) {
+      return storedCart.reduce((total, item) => total + item.quantity, 0);
+    }
+    return 0;
+  };
+
+  useEffect(() => {
+    setCartItemsCount(calculateCartItemsCount());
+
+    const intervalId = setInterval(() => {
+      setCartItemsCount(calculateCartItemsCount());
+    });
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem('isAuthenticated');
@@ -42,26 +63,33 @@ const Navbar = () => {
                 Food Items
               </Link>
             </li>
-          
-            {!isAuthenticated ? (
+            {isAuthenticated ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/checkout">
+                    <i className="fas fa-shopping-cart"></i>
+                    {cartItemsCount > 0 && (
+                      <span className="badge bg-danger ms-1">
+                        {cartItemsCount}
+                      </span>
+                    )}
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <button
+                    className="btn btn-link nav-link"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
               <li className="nav-item">
                 <Link className="nav-link" to="/login">
                   Login Here
                 </Link>
               </li>
-            ) : (
-              <>
-              <li className="nav-item">
-              <Link className="nav-link" to="/checkout">
-                  Your Cart
-                </Link>
-              </li>
-              <li className="nav-item">
-              <button className="btn btn-link nav-link" onClick={handleLogout}>
-                Logout
-              </button>
-            </li>
-            </>
             )}
           </ul>
         </div>
